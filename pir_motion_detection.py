5595e5aeebdb9cf0318d231f66708540906f4711
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2017-04-14 20:25:03 +0800

    Fixed styling

--- pir_motion_detection.py
+++ pir_motion_detection.py
@@ -10,44 +10,46 @@ GPIO.setmode(GPIO.BOARD)
 GPIO_PIR = 11
 GPIO.setup(GPIO_PIR, GPIO.IN)     # Read output from PIR motion sensor
 
-current_state = 0
-previous_state = 0
-
 
 # A helper for reading value on GPIO pin that is connected to PIR's output PIN
 def read_pir():
     return GPIO.input(GPIO_PIR)
 
+
 # Actions to execute when intruders are detected
 def on_intruders_detected():
-    # TODO: Implement actions against intruders. 
+    # TODO: Implement actions against intruders.
     #   Options:
-    #       1. Send push notification to iOS / Android phone. 
-    #       2. Send email 
-    #       3. Tweet 
-    #       4. Turn on camera module, then capture picture/short video against the intruders and sent it to iOS/Android Phones.
+    #       1. Send push notification to iOS / Android phone.
+    #       2. Send email
+    #       3. Tweet
+    #       4. Turn on camera module, then capture picture/short video against
+    #          the intruders and sent it to iOS/Android Phones
     #       5. Many more
 
 # Main program
-try:
-    print("Waiting for PIR to settle ...")
-    while read_pir() == 1:
+    try:
         current_state = 0
-        print("	Ready")
+        previous_state = 0
 
-    while True:
-        current_state = read_pir()
-        #print "D current_state= " + str(current_state)
-        if current_state == 1 and previous_state == 0:
-            print("	Intruders detected!")
-            # TODO: Do alerts or other actions here, for some time
-            on_intruders_detected()
-            previous_state = 1
-        elif current_state == 0 and previous_state == 1:
+        print("Waiting for PIR to settle ...")
+        while read_pir() == 1:
+            current_state = 0
             print("	Ready")
-            previous_state = 0
-            time.sleep(0.001)
 
-except KeyboardInterrupt:
-    print("	Quit")
-    GPIO.cleanup()
+        while True:
+            current_state = read_pir()
+            #print "D current_state= " + str(current_state)
+            if current_state == 1 and previous_state == 0:
+                print("	Intruders detected!")
+                # TODO: Do alerts or other actions here, for some time
+                on_intruders_detected()
+                previous_state = 1
+            elif current_state == 0 and previous_state == 1:
+                print("	Ready")
+                previous_state = 0
+                time.sleep(0.001)
+
+    except KeyboardInterrupt:
+        print("	Quit")
+        GPIO.cleanup()
