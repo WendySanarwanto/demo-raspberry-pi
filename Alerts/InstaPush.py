8e35727cff07c4a7c547717eca1026c4276877ac
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-04-15 15:04:57 +0800

    Refactored logic which made POST request to API by moved them into a seperate class.

--- Alerts/InstaPush.py
+++ Alerts/InstaPush.py
@@ -2,7 +2,7 @@ import pycurl
 import json
 from StringIO import StringIO
 # from io import StringIO
-
+from ..Utility import Curl
 
 class InstaPush:
     """
@@ -14,35 +14,22 @@ class InstaPush:
         self.app_id = ""  # Enter your instapush's app id
         self.app_secret = ""  # Enter your instapush's secret key
         self.push_event = "IntruderAlert"
-        self.api_url = "https://api.instapush.im/v1/post"
+        #self.api_url = "https://api.instapush.im/v1/post"
+        self.api_url = "https://api.instapush.im/v1"
+        self.push_notification_path = "/post"
 
     def request_push_notification(self, message_to_push):
         """
             Send request to Instapush, for sending alert notification
             to registered phones
         """
-        response_buffer = StringIO()
-
         request = {}
         request['event'] = self.push_event
         request['trackers'] = {}
         request['trackers']['message'] = message_to_push
-        postfields = json.dumps(request)
 
-        curl = pycurl.Curl()
-        curl.setopt(curl.URL, self.api_url)
-        curl.setopt(curl.HTTPHEADER, ['x-instapush-appid: ' + self.app_id, 'x-instapush-appsecret: ' + self.app_secret, 'Content-Type: application/json'])
-        curl.setopt(curl.POSTFIELDS, postfields)
-        curl.setopt(curl.WRITEFUNCTION, response_buffer.write)
-        curl.setopt(curl.VERBOSE, True)
-        curl.setopt(curl.IPRESOLVE, curl.IPRESOLVE_V4)
+        http_headers = ['x-instapush-appid: ' + self.app_id, 'x-instapush-appsecret: ' + self.app_secret, 'Content-Type: application/json']
 
-        curl.perform()
+        curl = Curl(api_url, True, True)
 
-        response = response_buffer.getvalue()
-        response_buffer.truncate(0)
-        response_buffer.seek(0)
-
-        curl.close()
-
-        return response
+        return curl.do_post(request, http_headers, self.push_notification_path)
