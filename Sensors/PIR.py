f5baf28abe0e9878d650909422d4f5e6234ed944
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-04-15 11:00:24 +0800

    Removed self from GPIO.setup call

--- Sensors/PIR.py
+++ Sensors/PIR.py
@@ -12,7 +12,7 @@ class PIR:
         GPIO.setwarnings(gpio_warnings)
         GPIO.setmode(gpio_mode)
         self.GPIO_PIR = 11  # GPIO Pin's number, attached to PIR's output rail.
-        self.GPIO.setup(self.GPIO_PIR, GPIO.IN)  # Read output from PIR motion sensor
+        GPIO.setup(self.GPIO_PIR, GPIO.IN)  # Read output from PIR motion sensor
 
     def do_read(self):
         """ Reading value on GPIO pin, that is connector to PIR's output rail. """
